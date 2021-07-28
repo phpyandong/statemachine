32b6583dad5f14ef17610537ec69b9dc14d29a67
zhangyandong <zhangyandong@popmart.com>
2021-07-28 20:40:49 +0800

    状态机触发

--- engine/StateMachine.go
+++ engine/StateMachine.go
@@ -1,37 +1,27 @@
 package engine
-/**
-  private final GlobalContext globalContext;
 
-    private final StateConfiguration[] machineConfiguration;
+import (
+	"statemachine/engine/configration"
+	"statemachine/engine/context"
+	"statemachine/engine/core"
+)
 
-    private final ScheduledExecutorService timer = new ScheduledThreadPoolExecutor(1);
-    private Future<?> delayTransitionTask;
-
-    private State currentState;
-    private StateConfiguration currentStateConfiguration;
-
-    /**
-     * 触发状态变更
-     * @param trigger
-     * @return
-     * @throws UnknownTriggerException
-     * @throws MissingStateConfigurationException
-*/
-public synchronized State fire(final Event trigger) throws UnknownTriggerException, MissingStateConfigurationException {
-/**
- * 获取下一状态
- */
-currentState = currentStateConfiguration.fire(trigger, machineConfiguration,globalContext);
-/**
- * 获取下一状态的配置信息
- */
-currentStateConfiguration = machineConfiguration[currentState.getPosition()];
-
-cleanFormerDelayTransition();
-/**
- * 开始延时传输
- */
-startDelayTransition(currentStateConfiguration);
-return currentState ;
+type StateMachine struct {
+	GlobalContext             context.IGlobalContext
+	MachineConfiguration      []configration.StateConfiguration
+	CurrentState              core.IState
+	CurrentStateConfiguration configration.StateConfiguration
 }
- */
+
+func (stateMachine *StateMachine) Fire(trigger core.IEvent) core.IState {
+	//获取下一状态
+	currentState := stateMachine.CurrentStateConfiguration.Fire(trigger, stateMachine.MachineConfiguration, stateMachine.GlobalContext)
+	currentStateConfiguration := stateMachine.MachineConfiguration[currentState.GetPosition()]
+	//cleanFormerDelayTransition();
+	/**
+	 * 开始延时传输
+	 */
+	//startDelayTransition(currentStateConfiguration);
+
+	return currentState
+}
